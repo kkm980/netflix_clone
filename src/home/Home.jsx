b2709007eae13cd837d9411b3a36f723335d7902
kkm980 <kantkrishna751@gmail.com>
2021-11-06 21:14:52 +0530

    navbar added

---
+++ src/home/Home.jsx
@@ -0,0 +1,14 @@
+import "./Home.scss"
+
+import React from 'react'
+import Navbar from "../helpers/navbar/Navbar"
+
+function Home() {
+    return (
+        <div className="home">
+            <Navbar/>
+        </div>
+    )
+}
+
+export default Home
