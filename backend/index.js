73a82863e96ee0756247a6f97acf13e8afd468d1
kkm980 <kantkrishna751@gmail.com>
2021-11-24 20:23:25 +0530

    connected with database

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,17 @@
 const express= require("express");
 const app=express();
 const mongoose=require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+})
+.then(()=>{console.log("database connected")})
+.catch((err)=>{console.log(err)});
+
 app.listen(8700, ()=>{
     console.log("got you");
-})
+});
